9d8091bdea100c861f402a155c5d325441f00b75
Skuiggly <skuigglymail@gmail.com>
2023-09-19 03:52:33 +0200

    build: set up vite in lib mode

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    outDir: "dist",
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, "/src/index.ts"),
+      name: "useStore",
+      // the proper extensions will be added
+      fileName: "use-store",
+      formats: ["es", "cjs", "iife", "umd"],
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["react"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: "react",
+        },
+      },
+    },
+  },
+  plugins: [dts({ include: "./src/lib" })],
+});
